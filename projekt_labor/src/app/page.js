04431b4ba5c49427bfb36bba7414ae61a0c64266
kriszR <varga.kristof333@gmail.com>
2024-09-23 19:40:25 +0200

    Initial commit
    Started project

---
+++ projekt_labor/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex min-h-screen w-full justify-center items-center">
+    <input type="text"></input><button type="button">Search</button>
+   </div>
+  );
+}
