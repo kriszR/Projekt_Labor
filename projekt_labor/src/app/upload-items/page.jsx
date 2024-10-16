0441539897a1b3f9b97516e54ab85e7ebf362c0c
VeghAttiIMC <77452379+VeghAtti@users.noreply.github.com>
2024-10-16 21:34:14 +0200

    Data save to localstorage + more details on main page

--- projekt_labor/src/app/upload-items/page.jsx
+++ projekt_labor/src/app/upload-items/page.jsx
@@ -1,222 +1,3 @@
-/*'use client';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { useState, useEffect } from 'react';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import Alert from '@/components/Alert';
-import Loading from '@/components/Loading';
-
-async function UploadProduct(
-  name,
-  category,
-  description,
-  price,
-  store,
-  setAlert,
-  setLoading
-) {
-  try {
-    setLoading(true);
-
-    if (!name || !category || !price || !store)
-      throw Error('Please fill out all the required fields!');
-
-    const request = await fetch('http://localhost:3000/api/products', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, category, description, price, store }),
-    });
-    console.log(request);
-    if (!request.ok) throw Error("Couldn't add item, try again!");
-
-    setAlert({ message: 'Item successfully added!', type: 'success' });
-  } catch (err) {
-    setAlert({ message: err.message, type: 'error' });
-  } finally {
-    setLoading(false);
-  }
-}
-
-export default function UploadPage() {
-  const [name, setName] = useState('');
-  const [category, setCategory] = useState('');
-  const [description, setDescription] = useState();
-  const [price, setPrice] = useState(0);
-  const [store, setStore] = useState('');
-  const [date, setDate] = useState('');
-  const [alert, setAlert] = useState({ message: '', type: '' });
-  const [loading, setLoading] = useState(false);
-
-  const [newCategory, setNewCategory] = useState('');
-  const [showInput, setShowInput] = useState(false); 
-
-  const currentDate = new Date().toISOString().split('T')[0];
-
-  useEffect(() => {
-    if (alert.type === 'success') {
-      setName('');
-      setCategory('');
-      setDescription('');
-      setPrice(0);
-      setStore('');
-      setTimeout(() => setAlert({ message: '', type: '' }), 3000);
-    }
-  }, [alert]);
-
-  const handleCategoryChange = (value) => {
-    if (value === 'other') {
-      setShowInput(true);
-    } else {
-      setCategory(value);
-      setShowInput(false);
-    }
-  };
-
-  return (
-    <main>
-      <div className='grid w-full max-w-sm items-center gap-3'>
-        <Label htmlFor='name'>Name *</Label>
-        <Input
-          type='input'
-          id='name'
-          placeholder='Name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <Label htmlFor='category'>Category *</Label>
-        <Select
-          className='w-full'
-          onValueChange={handleCategoryChange}
-          value={category}
-          id='category'
-        >
-          <SelectTrigger>
-            <SelectValue placeholder='Select Category' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value='other'>Other</SelectItem>
-              <SelectLabel>North America</SelectLabel>
-              <SelectItem value='est'>Eastern Standard Time (EST)</SelectItem>
-              <SelectItem value='cst'>Central Standard Time (CST)</SelectItem>
-              <SelectItem value='mst'>Mountain Standard Time (MST)</SelectItem>
-              <SelectItem value='pst'>Pacific Standard Time (PST)</SelectItem>
-              <SelectItem value='akst'>Alaska Standard Time (AKST)</SelectItem>
-              <SelectItem value='hst'>Hawaii Standard Time (HST)</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-
-        {showInput && (
-          <div>
-            <Input
-              type='text'
-              placeholder='Please add a new category'
-              value={newCategory}
-              onChange={(e) => setNewCategory(e.target.value)}
-            />
-            <Button  className='mt-2'>
-              Add
-            </Button>
-          </div>
-        )}
-
-        <Label htmlFor='description'>Description</Label>
-        <Input
-          type='input'
-          id='description'
-          placeholder='Description'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-
-        <Label htmlFor='price'>Price *</Label>
-        <div>
-          <Input
-            type='number'
-            className='inline-flex w-[100px]'
-            min={1}
-            max={99999999}
-            id='price'
-            placeholder='Price'
-            value={price}
-            onChange={(e) => setPrice(parseInt(e.target.value))}
-          />
-          <span className='ml-2'>Ft</span>
-        </div>
-
-        <Label htmlFor='date'>Date</Label>
-        <Input
-          type='date'
-          className='w-fit'
-          id='date'
-          min={'2000-01-01'}
-          max={currentDate}
-          value={date || currentDate}
-          onChange={(e) => setDate(e.target.value)}
-        />
-
-        <Label htmlFor='store'>Store *</Label>
-        <Select
-          className='w-full'
-          onValueChange={setStore}
-          value={store}
-          id='store'
-        >
-          <SelectTrigger>
-            <SelectValue placeholder='Select Store' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>North America</SelectLabel>
-              <SelectItem value='est'>Eastern Standard Time (EST)</SelectItem>
-              <SelectItem value='cst'>Central Standard Time (CST)</SelectItem>
-              <SelectItem value='mst'>Mountain Standard Time (MST)</SelectItem>
-              <SelectItem value='pst'>Pacific Standard Time (PST)</SelectItem>
-              <SelectItem value='akst'>Alaska Standard Time (AKST)</SelectItem>
-              <SelectItem value='hst'>Hawaii Standard Time (HST)</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-
-        <Button
-          className='justify-self-center'
-          onClick={() =>
-            UploadProduct(
-              name,
-              category,
-              description,
-              price,
-              store,
-              setAlert,
-              setLoading
-            )
-          }
-        >
-          Upload Item
-        </Button>
-
-        {(loading && <Loading />) ||
-          (alert.message && (
-            <Alert type={alert.type} message={alert.message} />
-          ))}
-      </div>
-    </main>
-  );
-}*/
-
 'use client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -310,27 +91,73 @@ export default function UploadPage() {
     }
   };
 
-  const [categories, setCategories] = useState(['Electronics','Books','Clothing',]);
+  const [categories, setCategories] = useState([
+    'Water',
+    'White Bread',
+    'Milk',
+  ]);
   const [newCategory, setNewCategory] = useState('');
 
   const [stores, setStores] = useState(['Aldi', 'Lidl', 'Tesco']);
   const [newStore, setNewStore] = useState('');
 
-  const addCategory = () => {
+  
+   useEffect(() => {
+    const savedCategories = localStorage.getItem('categories');
+    const savedStores = localStorage.getItem('stores');
+
+    if (savedCategories) setCategories(JSON.parse(savedCategories));
+    if (savedStores) setStores(JSON.parse(savedStores));
+  }, []);
+
+
+  /*const addCategory = () => {
     if (newCategory && !categories.includes(newCategory)) {
       setCategories([...categories, newCategory]);
+      setCategory(newCategory);
       setNewCategory('');
+      setShowInput(false);
     } else {
-      alert('The category already exits!');
+      alert('The category already exists!');
+    }
+  };*/
+
+  const addCategory = () => {
+    if (newCategory && !categories.includes(newCategory)) {
+      const updatedCategories = [...categories, newCategory];
+      setCategories(updatedCategories);
+      setCategory(newCategory);
+      setNewCategory('');
+      setShowInput(false);
+      localStorage.setItem('categories', JSON.stringify(updatedCategories));
+    } else {
+      alert('The category already exists!');
     }
   };
 
-  const addStore = () => {
+
+  /*const addStore = () => {
     if (newStore && !stores.includes(newStore)) {
       setStores([...stores, newStore]);
+      setStore(newStore); 
       setNewStore('');
+      setShowInputStore(false);
     } else {
-      alert('The store already exits!');
+      alert('The store already exists!');
+    }
+  };*/
+
+
+  const addStore = () => {
+    if (newStore && !stores.includes(newStore)) {
+      const updatedStores = [...stores, newStore];
+      setStores(updatedStores);
+      setStore(newStore);
+      setNewStore('');
+      setShowInputStore(false);
+      localStorage.setItem('stores', JSON.stringify(updatedStores));
+    } else {
+      alert('The store already exists!');
     }
   };
 
@@ -358,12 +185,12 @@ export default function UploadPage() {
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
+            <SelectItem value='other'>Other</SelectItem>
               {categories.map((category, index) => (
                 <SelectItem key={index} value={category}>
                   {category}{' '}
                 </SelectItem>
               ))}
-              <SelectItem value='other'>Other</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
@@ -429,12 +256,12 @@ export default function UploadPage() {
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
+            <SelectItem value='other'>Other</SelectItem>
               {stores.map((stores, index) => (
                 <SelectItem key={index} value={stores}>
                   {stores}{' '}
                 </SelectItem>
               ))}
-              <SelectItem value='other'>Other</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
