caeb19ce2d3aab14ceb34277e1493bc1066f1cfd
Straxus01 <akos.nagy0317@gmail.com>
2024-10-23 21:34:47 +0200

    login/register_2

--- projekt_labor/src/app/layout.jsx
+++ projekt_labor/src/app/layout.jsx
@@ -1,15 +1,18 @@
+import Providers from '@/components/Providers';
 import './globals.scss';
 import Header from '@/components/Header';
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <Providers>
       <body className={`antialiased`}>
         <>
           <Header />
           {children}
         </>
       </body>
+      </Providers>
     </html>
   );
 }
